fe0d405702b1caa7ca9a21e77972ac88176ecaf2
MW_Lee <21900505@handong.edu>
2020-11-26 16:58:56 +0900

    0.1version

---
+++ Boll2D/Assets/code/망토적.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 망토적 : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float moveSpeed = 3;
+    public int nextMove;
+    SpriteRenderer spriteRenderer;//방향
+
+    Animator anim;
+
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        Invoke("random", 5);
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+
+        //벽 확인
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rigid.velocity.y < 0)
+        {
+            if (rayHit.collider == null)
+            {
+                Return();
+            }
+        }
+        //이동
+        Vector3 moveVelocity = Vector3.zero;
+        if (nextMove == -1)
+        {
+            moveVelocity = Vector3.left;
+
+        }
+        else if (nextMove == 0)
+        {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
+
+        }
+        else if (nextMove == 1)
+        {
+            moveVelocity = Vector3.right;
+
+        }
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+
+
+
+    }
+    void random()
+    {
+        nextMove = Random.Range(-1, 2);
+        if (nextMove == 0)
+            anim.SetBool("walking", false);
+        else
+            anim.SetBool("walking", true);
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("random", nextThinkTime);
+
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == -1;
+        }
+
+    }
+
+    void Return()
+    {
+        nextMove *= -1;
+        if (nextMove != 0)
+            anim.SetBool("walking", true);
+        else
+            anim.SetBool("walking", false);
+        spriteRenderer.flipX = nextMove == -1;
+
+        CancelInvoke();
+        Invoke("random", 2);
+    }
+}
---
+++ Boll2D/Assets/code/히어로.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 히어로 : MonoBehaviour
+{
+    public float maxSpeed = 0;
+    public float jumpPower = 18;
+    public int jumpcount = 1;
+    public float moveSpeed = 10f;
+    public float damage_time = 1f; //무적 시간
+    Rigidbody2D rigid;//rigid
+    SpriteRenderer spriteRenderer;//방향
+    Animator anim;//애니메이션
+
+    //---
+    Vector3 movement;
+    int jumpcnt;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //점프
+        jump();
+    }
+
+    void FixedUpdate()
+    {
+        //Move_0(); 사용 안함
+        Move_1();
+    }
+    void Move_0()
+    {
+
+        //옛날버전 move
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+
+        if (h < 0f)
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+        else if (h > 0f)
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        }
+    }
+    void Move_1()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+            anim.SetBool("iswalking", true);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+            anim.SetBool("iswalking", true);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
+            anim.SetBool("iswalking", false);
+        }
+
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+    }
+    void jump()
+    {
+        //점프
+        if (Input.GetButtonDown("Jump") && jumpcnt > 0)
+        {
+
+            rigid.velocity = Vector2.up * jumpPower;
+            anim.SetBool("isjumping", true);
+            anim.SetBool("iswalking", false);
+            jumpcnt--;
+        }
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rigid.velocity.y < 0)
+        {
+            if (rayHit.collider != null)
+            {
+                anim.SetBool("isjumping", false);
+                jumpcnt = jumpcount;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            OnDamaged(collision.transform.position);
+        }
+    }
+    void OnDamaged(Vector2 targetPos)
+    {
+        gameObject.layer = 11;
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1) * 8, ForceMode2D.Impulse);
+
+        anim.SetTrigger("doDamaged");
+
+
+        Invoke("OffDamaged", damage_time);
+    }
+    void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+}
